499cbe9f12612ee8c9140f25c1c780134bab0061
Icham Jaber <ijaber@student.42.fr>
2024-05-28 16:15:59 +0200

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 12:31:43 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/28 15:12:26 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/28 16:15:40 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int	main(int ac, char **av)
 {
+		
 	if (ac < 2)
 		ft_putstr_fd("Usage : ./so_long <map.ber> \n", 2);
-	if (ac > 2)
+	else if (ac > 2)
 		ft_pustr_fd("Too many args... \n", 2);
-	else
-	{
-		fd = open(av[1]);
-	}
+	else if (ac == 2 && check_ber_map(av[1]))
+		ft_putstr_fd("Wrong File or Map Name. \n")
+	start;
+		
 	return (0);
 }
